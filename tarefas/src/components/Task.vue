ab669fe0ba42fcd2fe84b83aaec6509cfed5c434
Emerson Braun <emersonfbraun@gmail.com>
2019-04-08 17:00:32 -0300

    finalizado projeto - todos

---
+++ tarefas/src/components/Task.vue
@@ -0,0 +1,56 @@
+<template>
+    <div 
+    @click="$emit('taskStateChanged', task)"
+    class="col-md-3 card task" :class="stateClass">
+        <div class="card-head">
+            <span @click.stop="$emit('taskDeleted',task)" class="close">x</span>
+        </div>
+        <div class="card-body">
+            {{ task.name }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props:{
+            task: {type: Object, required: true}
+        },
+        computed: {
+            stateClass(){
+                return{
+                    'bg-danger': this.task.pending,
+                    'bg-success': !this.task.pending
+                }
+            }
+        },
+    }
+</script>
+
+<style scoped>
+    .task {
+        box-sizing: border-box;
+        min-width: 200px;
+        height: 100px;
+        padding: 10px;
+        border-radius: 8px;
+        font-size: 1.5rem;
+        font-weight: 300;
+        cursor: pointer;
+        user-select: none;
+    }
+    .bg-danger{
+        border-left: 12px solid #b73229;
+    }
+    .bg-success{
+        color: #ddd;
+        border-left: 12px solid #0a8f08;
+        text-decoration: line-through;
+    }
+    .bg-danger.close{
+       background-color: #b73229; 
+    }
+    .bg-success.close{
+        background-color: #0a8f0a;
+    }
+</style>
---
+++ tarefas/src/components/TaskGrid.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="task-grid">
+        <template v-if="tasks.length">
+            <div class="row">
+                <template>
+                    <Task 
+                    v-for="(task, i) in tasks" 
+                    :key="task.name" :task="task"
+                    @taskDeleted="$emit('taskDeleted', i)"
+                    @taskStateChanged="$emit('taskStateChanged',i)"
+                    />
+                </template>
+            </div>
+        </template>
+        <p v-else class="no-task">Sua vida está em dia!</p>
+    </div>
+</template>
+
+<script>
+import Task from './Task'
+    export default {
+        components: { Task },
+        props: {
+            tasks: { type: Array, required: true}
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
