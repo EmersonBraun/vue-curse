b8d60f2871b90552dbd4241f2cfba546f9086e02
Emerson Braun <emersonfbraun@gmail.com>
2019-05-06 17:43:28 -0300

    exercicios filters and mixins

---
+++ filtros-mixins-exercicios/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+	<div id="app">
+		<h1>Filtros & Mixins</h1>
+		<hr>
+		<p>{{ cpf | cpf | inverter}}</p>
+		<input type="text" :value="cpf | cpf ">
+		<v-frutas></v-frutas>
+	</div>
+</template>
+
+<script>
+import Frutas from './Frutas'
+import FrutasMixins from './FrutasMixins'
+export default {
+	components: {'v-frutas':Frutas},
+	mixins: [FrutasMixins],
+	data() {
+		return {
+			cpf: '07326994951',
+		}
+	},
+	filters: {
+		cpf(valor){
+			if(!valor) return ''
+			const arr = valor.split('')
+			arr.splice(3,0,'.')
+			arr.splice(7,0,'.')
+			arr.splice(11,0,'-')
+			return arr.join('')
+		}
+	},
+}
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+	font-size: 2.5rem;
+}
+input{
+	font-size: 2.5rem;
+}
+</style>
---
+++ filtros-mixins-exercicios/src/Frutas.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="(f,index) in frutas" :key="index">
+                {{ f }}
+            </li>
+        </ul>
+        <hr>
+        <input type="text" v-model="fruta" @keydown.enter="add">
+        <button @click="add">Add</button>
+    </div>
+</template>
+
+<script>
+import FrutasMixins from './FrutasMixins'
+    export default {
+       mixins: [FrutasMixins],
+    }
+</script>
+
+<style scoped>
+
+</style>
