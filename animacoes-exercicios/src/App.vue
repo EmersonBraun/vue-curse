435eba137476e0101f3ef9a9c30d25ef3c0252e5
Emerson Braun <emersonfbraun@gmail.com>
2019-05-15 15:03:55 -0300

    terminado exercicios

--- animacoes-exercicios/src/App.vue
+++ animacoes-exercicios/src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="app" class="container-fluid">
 		<h1>Animações</h1>
-    <hr>
+    <!--<hr>
     <button class="btn btn-primary mb-4" @click="exibir = !exibir">Mostrar mensagem</button>
     
-    <transition name="fade" appear>
+     <transition name="fade" appear>
       <v-alert v-if="exibir" type="info" duration="3000" dismissable>{{ msg }}</v-alert>
     </transition>
 
@@ -18,19 +18,41 @@
       <v-alert v-if="exibir" type="info" duration="3000" dismissable>{{ msg }}</v-alert>
     </transition>
 
-    <!--<v-select v-model="tipoAnimacao">
-      <v-option value="fade">Fade</v-option>
-      <v-option value="slide">Slide</v-option>
-    </v-select>
+    <select v-model="tipoAnimacao">
+      <option value="fade">Fade</option>
+      <option value="slide">Slide</option>
+    </select>
 
     <transition :name="tipoAnimacao" appear>
       <v-alert v-if="exibir" type="info" duration="3000" dismissable>{{ msg }}</v-alert>
-    </transition>-->
+    </transition>
 
     <transition :name="tipoAnimacao" mode="out-in">
       <v-alert v-if="exibir" type="info" duration="3000" dismissable>{{ msg }}</v-alert>
       <v-alert v-else type="warning" duration="3000" dismissable>{{ msg }}</v-alert>
     </transition>
+
+    <hr> -->
+    <!--<button class="btn btn-success" @click="exibir2 = !exibir2">Alternar</button>
+     <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @enter-cancelled="enterCancelled"
+      
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+      @leave-cancelled="leaveCancelled">
+       <div v-if="exibir2" class="caixa"></div>
+    </transition>-->
+    <hr>
+    <button class="btn btn-primary mb-4" @click="add">Adicionar</button>
+    <transition-group name="fade">
+      <ul class="list-group" v-for="(aluno, i) in alunos" :key="aluno">
+        <li class="list-group-item" style="cursor: pointer" @click="remove(i)">{{ aluno }}</li>
+      </ul>
+    </transition-group>
   </div>
 </template>
 
@@ -41,9 +63,64 @@ export default {
     return {
       msg: 'Uma mensagem de informação para o usuário',
       exibir: false,
+      exibir2: true,
       tipoAnimacao: 'fade',
+      largura: 0,
+      alunos: ['Pedro','Tiago','João']
     }
-  }
+  },
+  methods: {
+    animar(el, done, negativo) {
+      let rodada = 1
+      const tempo = setInterval(() => {
+        const novaLargura = this.largura + 
+        (negativo ? -rodada * 10 : rodada * 10)
+        el.style.width = `${novaLargura}px`
+        rodada ++
+        if(rodada > 30){
+          clearInterval(tempo)
+          done()
+        }
+      },20)
+      done()
+    },
+    add(){
+      let str = Math.random().toString(36).substring(2)
+      this.alunos.push(str)
+    },
+    remove(i){
+      this.alunos.splice(i,1)
+    },
+    beforeEnter(el){
+      // console.log('beforeEnter')
+      this.largura = 0
+      el.style.width = `${this.largura}px`
+    },
+    enter(el, done){
+      this.animar(el, done, false)
+    },
+    afterEnter(el){
+      console.log('afterEnter')
+    },
+    enterCancelled(el){
+      console.log('enterCancelled')
+    },
+
+    beforeLeave(el){
+      // console.log('beforeLeave')
+      this.largura = 300
+      el.style.width = `${this.largura}px`
+    },
+    leave(el, done){
+      this.animar(el, done, true)
+    },
+    afterLeave(el){
+      console.log('afterLeave')
+    },
+    leaveCancelled(el){
+      console.log('leaveCancelled')
+    },
+  },
 }
 </script>
 
@@ -58,21 +135,21 @@ export default {
 	font-size: 1.5rem;
 }
 
-.fade-enter, .fade-leave-to{
-  opacity: 0;
-}
-.fade-enter-active,.fade-leave-active {
-  transition: opacity 2s;
+.caixa {
+  width: 300px;
+  height: 100px;
+  margin: 30px auto;
+  background-color: lightblue;
 }
 
 @keyframes slide-in {
-  from { transform: translateX(200px); }
+  from { transform: translateX(100px); }
   to { transform: translateX(0); }
 }
 
 @keyframes slide-out {
   from { transform: translateX(0); }
-  to { transform: translateX(200px); }
+  to { transform: translateX(100px); }
 }
 
 .slide-enter-active {
@@ -80,12 +157,18 @@ export default {
   transition: opacity 2s;
 }
 .slide-leave-active {
+  position: absolute;
+  width: 100%;
   animation: slide-out 2s linear;
   transition: opacity 2s;
 }
 
 .slide-enter, .slide-leave-to{
   opacity: 0;
+}
+
+.slide-move{
+  transition: transform 1s;
 }
 /*
 .x-enter {}
--- animacoes-exercicios/src/main.js
+++ animacoes-exercicios/src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import './style.css'
+import './animation.css'
 
 Vue.config.productionTip = false
 
